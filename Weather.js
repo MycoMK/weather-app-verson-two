4d03ee3a3f1cb4b0d70ad73734114e780a2d3939
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2020-07-18 10:08:12 -0700

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,16 @@
+var weather = "Sunday";
+if (weather == "Monday") {
+  document.write("It is going to rain");
+} else if (weather == "Tuesday") {
+  document.write("It is going to snow");
+} else if (weather == "wednesday") {
+  document.write("It is going to be windy");
+} else if (weather == "Thursday") {
+  document.write("It is going to be cloudy");
+} else if (weather == "Friday") {
+  document.write("It is going to be snowing heavly 5.6ft");
+} else if (weather == "Saturday") {
+  document.write("It is going to be sunny all day");
+} else if (weather == "Sunday") {
+  document.write("It is going to be sunny with a chance of rain in the night");
+}
